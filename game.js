87681f79c264fcfb91705555c12e4f8c09194695
Chloe Urisohn <chloeurisohn@gmail.com>
2021-03-20 16:36:25 +0000

    init game class

--- game.js
+++ game.js
@@ -11,14 +11,6 @@ const config = {
   type: Phaser.AUTO,
   width: 800,
   height: 500,
-  physics: {
-    default: 'arcade',
-    arcade: {
-        gravity: {
-            y: 200
-        }
-    }
-  },
 };
 
 const game = new Phaser.Game(config);
