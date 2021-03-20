e6d43088478d8e4ee84731acf4db8e9f901a11ea
Chloe Urisohn <chloeurisohn@gmail.com>
2021-03-20 10:36:53 +0000

    init

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+// Initialise the game object
 const config = {
     type: Phaser.AUTO,
     width: 800,
@@ -12,12 +13,14 @@ const config = {
     },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
 const game = new Phaser.Game(config);
 
+// Preload Function
 function preload() {
     this.load.image('sky', 'assets/sky.png');
     this.load.spritesheet('witch',
@@ -29,14 +32,26 @@ function preload() {
 
 }
 
+// Create Function
 function create() {
     this.anims.create({
-        key: "fly",
+        key: 'fly',
         frameRate: 7,
-        frames: this.anims.generateFrameNumbers("witch", { start: 1, end: 6 }),
+        frames: this.anims.generateFrameNumbers('witch', { start: 1, end: 6 }),
         repeat: -1
     });
     this.add.image(400, 250, 'sky');
-    const player = this.add.sprite(100, 100, 'witch');
+    player = this.physics.add.sprite(100, 100, 'witch');
+    player.setBounce(0.2);
+    player.setCollideWorldBounds(true);
     player.play('fly');
+}
+
+// Update Function
+function update() {
+    cursors = this.input.keyboard.createCursorKeys();
+    if (cursors.up.isDown) {
+        player.setVelocityY(-330);
+        player.anims.play('fly', true);
+    };
 }
