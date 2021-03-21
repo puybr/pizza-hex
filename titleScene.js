3f13b5d3f8754b5a1dbe6ea6519d42f38f969e39
Chloe Urisohn <chloeurisohn@gmail.com>
2021-03-21 13:28:33 +0000

    add title scene

--- titleScene.js
+++ titleScene.js
@@ -10,18 +10,25 @@ class TitleScene extends Phaser.Scene {
     preload() {
         this.load.image('title', 'assets/title.png');
         this.load.image('start', 'assets/start.png');
-        this.load.image('pentagram', 'assets/pentagram.png');
+        this.load.image('ghostgoblin', 'assets/ghostgoblin.png');
     }
 
     create() {
-        this.add.image(390, 250, 'title');
-        // this.add.image(400, 150, 'pentagram');
-        const text = this.add.image(400, 350, 'start');
-        text.setInteractive({useHandCursor: true});
-        text.on('pointerdown', () => this.clickButton());
+        const title = this.add.image(390, 250, 'title');
+        this.add.image(400, 150, 'ghostgoblin');
+        this.add.image(400, 350, 'start');
+        title.setInteractive({useHandCursor: true});
+        title.on('pointerdown', () => this.clickButton());
     }
     clickButton() {
         this.scene.switch('gameScene');
+    }
+
+    update() {
+        this.cursors = this.input.keyboard.createCursorKeys();
+        if (this.cursors.space.isDown) {
+            this.scene.switch('gameScene');
+        }
     }
 
 }
